2dff353f8b06649fcfdd2342411ad8cb8c3e94ec
cptdtbk <20521444@gm.uit.edu.vn>
2022-12-01 01:32:27 +0700

    cors

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.use(session({
     // httpOnly: false
 })
 );
-
+app.use(cors())
 app.use((req, res, next) => {
     const corsWhitelist = [
         'https://canteen-uit.netlify.app',
@@ -51,7 +51,6 @@ app.use((req, res, next) => {
         res.header('Access-Control-Allow-Origin', req.headers.origin);
         res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     }
-
     next();
 });
 
