0d8591f21b9a0bd8f37aeb8b7702c1945b925cf0
cptdtbk <20521444@gm.uit.edu.vn>
2022-12-03 11:36:35 +0700

    test order retrieval

--- index.js
+++ index.js
@@ -240,8 +240,8 @@ app.post('/sendorder', (req, res) => {
 
 app.get('/allorders', (req, res) => {
     let data = {
-        orderList,
-        orderDetail,
+        orderList: {},
+        orderDetail: {},
     };
     db.query('SELECT * FROM ordr WHERE 1', (err, result) => {
         if (err)
