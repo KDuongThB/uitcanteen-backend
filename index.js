b5adf08714b45944b45c55aed80f108a5d030fcc
cptdtbk <20521444@gm.uit.edu.vn>
2022-12-04 12:58:47 +0700

    order retrieval #n

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const session = require("express-session");
 
-const app = express.createServer();
+const app = express();
 
 const mysqlStore = require('express-mysql-session')(session);
 
